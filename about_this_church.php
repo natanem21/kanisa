f9009b522736764ba4eaf14781a37c57d295d61f
estifanos abebaw <zufan2kiflu@gmail.com>
2022-06-28 16:35:22 +0300

    delete files

--- about_this_church.php
+++
@@ -1,9 +0,0 @@
-<?php $title="church informaion";
-$direct="<a href ='index.php'> <img src='book.svg' alt='profile image' class='profile rect'>";
-include("header.php");
-?> 
-    <div class="c2">
-
-    </div>
-
-<?php include("footer.php");?>
