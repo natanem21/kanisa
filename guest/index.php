875c31d3efca53eda4fdfa4c9c6f39489dec503e
natanem21 <111303488+natanem21@users.noreply.github.com>
2022-08-15 11:06:02 -0700

    new

--- guest/index.php
+++ guest/index.php
@@ -21,19 +21,29 @@ include("header.php");
 
         <div class="go_left">
             <h1 style="background-color:red"><center>books</center></h1>
-            <a href="https://ethiopianorthodox.org/amharic/holybooks/orit/zelidet.pdf">ኦሪት ዘልደት</a></br>
-      
-            <a href="https://ethiopianorthodox.org/amharic/holybooks/orit/zetseat.pdf" >ኦሪት ዘጸአት</a></br>
-          <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/rute.pdf >ኦሪት ዘሩት</a></br>
-           <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/zelewaweyan.pdf >ኦሪት ዘሌዋውያን</a></br>
-<a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/zehulqe.pdf >ኦሪት ዘኁልቁ</a></br>
-         <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/zedagem.pdf >ኦሪት ዘዳግም</a></br>
- <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/joshua.pdf >ኦሪት ዘኢያሱ</a></br>
-          <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/mesafent.pdf>ኦሪት ዘመሳፍንት</a></br>
+   
+    <a href="https://ethiopianorthodox.org/amharic/holybooks/orit/zelidet.pdf">ኦሪት ዘልደት</a></br>
+   <a href="https://ethiopianorthodox.org/amharic/holybooks/orit/zetseat.pdf" >ኦሪት ዘጸአት</a></br>
+   <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/rute.pdf >ኦሪት ዘሩት</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/zelewaweyan.pdf >ኦሪት ዘሌዋውያን</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/zehulqe.pdf >ኦሪት ዘኁልቁ</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/zedagem.pdf >ኦሪት ዘዳግም</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/joshua.pdf >ኦሪት ዘኢያሱ</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/mesafent.pdf>ኦሪት ዘመሳፍንት</a></br>
   <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/samuel01.pdf>1ኛ መጽሐፈ ሳሙኤል</a></br>
-           <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/samuel02.pdf>2ኛ መጽሐፈ ሳሙኤል</a></br>
-<a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/negest01.pdf>1ኛ መጽሐፈ ነገሥት</a></br>
-         <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/negest02.pdf>2ኛ መጽሐፈ ነገሥት</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/samuel02.pdf>2ኛ መጽሐፈ ሳሙኤል</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/negest01.pdf>1ኛ መጽሐፈ ነገሥት</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/negest02.pdf>2ኛ መጽሐፈ ነገሥት</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/zena%20mewael%2002.pdf>2ኛ መጽሐፈ ዜና መዋዕል</a></br>
+  <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/ezera.pdf>መጽሐፈ ዕዝራ</a></br>
+ <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/nehimia.pdf>መጽሐፈ ነህምያ</a></br>
+ <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/aster.pdf>መጽሐፈ አስቴር</a></br>
+ <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/nebiat/eyob.pdf>መጽሐፈ ኢዮብ</a></br>
+ <a href="https://www.ethiopianorthodox.org/amharic/yeqolotbet/lessons/psalm.pdf>መዝሙረ ዳዊት</a></br>
+ <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/misale%20geeze%20and%20amahric.pdf>መጽሐፈ ምሳሌ</a></br>
+ <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/mekbib%20geeze%20and%20amahric.pdf>መጽሐፈ መክብብ</a></br>
+ <a href="https://www.ethiopianorthodox.org/amharic/holybooks/orit/mehailiye%20solomon%20geeze%20and%20amahric.pdf>መኀልየ መኀልይ</a></br>
+              
                    
             <a href="http://" target="_blank" rel="noopener noreferrer">book 3</a></br>
             <a href="http://" target="_blank" rel="noopener noreferrer">book 4</a></br>
